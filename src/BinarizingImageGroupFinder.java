dbe487857d4cce77d5c1a40690fd9b47a04ebacf
Butenkite <butenkite@gmail.com>
2025-10-20 15:56:01 -0700

    Quick psuedo code for findconnectedgroups

--- src/BinarizingImageGroupFinder.java
+++ src/BinarizingImageGroupFinder.java
@@ -37,6 +37,8 @@ public class BinarizingImageGroupFinder implements ImageGroupFinder {
      */
     @Override
     public List<Group> findConnectedGroups(BufferedImage image) {
+        // make an int[][] and binazrize it.
+        // make a list of groups by using BinaryGroupFinder
         return null;
     }
 }
