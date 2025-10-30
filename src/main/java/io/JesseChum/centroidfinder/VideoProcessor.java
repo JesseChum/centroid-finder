b0af0d5c4d4a33543ac9f143ba76166c36b6ce82
JesseChum <jessechum@icloud.com>
2025-10-29 20:01:23 -0700

    Added in important imports from javaFX as fields and a constructor method. the main method will be the next to implement

---
+++ src/main/java/io/JesseChum/centroidfinder/VideoProcessor.java
@@ -0,0 +1,26 @@
+package io.JesseChum.centroidfinder;
+
+import javafx.scene.image.Image;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.List;
+
+public class VideoProcessor {
+    private final String inputPath;
+    private final String outputCsv;
+    private final int targetColor;
+    private final int threshold;
+
+    public VideoProcessor(String inputPath, String outputCsv, int targetColor, int threshold) {
+        this.inputPath = inputPath;
+        this.outputCsv = outputCsv;
+        this.targetColor = targetColor;
+        this.threshold = threshold;
+    }
+}
