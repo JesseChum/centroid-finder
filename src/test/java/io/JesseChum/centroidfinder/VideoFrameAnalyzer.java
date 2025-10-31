784271c1afbc237b5602b2d7e68a6bb97532d8b6
JesseChum <jessechum@icloud.com>
2025-10-30 17:14:27 -0700

    Added test and fixed bugs

---
+++ src/test/java/io/JesseChum/centroidfinder/VideoFrameAnalyzer.java
@@ -0,0 +1,27 @@
+
+package io.JesseChum.centroidfinder;
+
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.util.List;
+import java.util.Optional;
+
+
+// import javafx.scene.effect.Light.Point;
+
+public class VideoFrameAnalyzer {
+    private final BinarizingImageGroupFinder centroidFinder;
+
+    public VideoFrameAnalyzer(ColorDistanceFinder distanceFinder, int targetColor, int threshold) {
+        ImageBinarizer binarizer = new DistanceImageBinarizer(distanceFinder, targetColor, threshold);
+        BinaryGroupFinder groupFinder = new DfsBinaryGroupFinder();
+        this.centroidFinder = new BinarizingImageGroupFinder(binarizer, groupFinder);
+    }
+
+    /** Returns largest group's centroid if any, else empty. */
+    public Optional<Coordinate> analyze(BufferedImage frame) {
+        List<Group> groups = centroidFinder.findConnectedGroups(frame);
+        if (groups.isEmpty()) return Optional.empty();
+        return Optional.of(groups.get(0).centroid());
+    }
+}
