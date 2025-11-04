3dc7b2b8adbba67c104f4933052c18156539e3a3
Niko_Butenko <174657090+Butenkite@users.noreply.github.com>
2025-11-04 11:19:46 -0800

    controller psuedocode

--- server/src/controllers/api.controller.js
+++ server/src/controllers/api.controller.js
@@ -1,10 +1,49 @@
 export const videoController = {
-    // getAllVideos
+    /* getAllVideos {
+    // try to grab json file
+    // if have json
+        // iterate through json file
+        // add all video names into a list
+        // return list
+    // else
+        // return 404 of no list
+}
 
-    // getThumbnail
+    * getThumbnail
+    // try to grab video
+    // if no video
+        // return 404 of no video
+    // else video
+        // send video to jar
+        // jar tries to return thumbnail
+        // if error
+            // return error status
+        // else thumbnail
+            // return status thumbnail
 
-    // startJob
+    * startJob
+    // try to grab video
+    // if no video
+        // return 404 of no video
+    // else video
+        // send video to jar
+        // if error in command
+            // return bad command status
+        // jar tries to start process
+        // if jar starts process
+            // return jobId status
+        // return job failed status
 
-    // getStatus
+    * getStatus
+    // if jobId does not exist
+        // return no jobid status
+    // if jobId exists
+        // if progress is error
+            // return error status
+        // if progress is processing
+            // return status of process
+        // if has csv
+            // return csv
+    */
 
 };
