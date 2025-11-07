bf93510e8088e24982ec853fb32043142f37dea9
JesseChum <jessechum@icloud.com>
2025-11-07 12:45:50 -0800

    added implementation to api.controller and thumbnail generator. both need to be looked over and still working on both. Thumbnail is the most important and hardest

---
+++ processor/src/main/java/io/JesseChum/centroidfinder/ThumbNailGenerator.java
@@ -0,0 +1,70 @@
+package io.JesseChum.centroidfinder;
+
+import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.Scene;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+
+public class ThumbNailGenerator extends Application {
+
+    private static String videoPath;
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            File videoFile = new File(videoPath);
+            if (!videoFile.exists()) {
+                System.err.println("Video not found: " + videoPath);
+                System.exit(1);
+            }
+            //1.recieve a video
+            // Load video
+            Media media = new Media(videoFile.toURI().toString());
+            MediaPlayer player = new MediaPlayer(media);
+            MediaView mediaView = new MediaView(player);
+
+            player.setOnReady(() -> {
+                try {
+                    //2. grab first frame
+                    // Wait until first frame is ready
+                    WritableImage image = new WritableImage(640, 360);
+                    mediaView.snapshot(null, image);
+
+                    // Save thumbnail
+                    File output = new File("thumbnail.png");
+                    ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", output);
+
+                    System.out.println("thumbnail.png");
+                    System.exit(0);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.exit(2);
+                }
+            });
+
+            player.play();
+            new Scene(new StackPane(mediaView), 640, 360);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(3);
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("Usage: ThumbNailGenerator <videoFile>");
+            System.exit(1);
+        }
+        videoPath = args[0];
+        launch(args);
+    }
+}
+    
--- server/src/controllers/api.controller.js
+++ server/src/controllers/api.controller.js
@@ -1,49 +1,85 @@
+import fs from "fs";
+import path from "path";
+
 export const videoController = {
-    /* getAllVideos {
-    // try to grab json file
-    // if have json
-        // iterate through json file
-        // add all video names into a list
-        // return list
-    // else
-        // return 404 of no list
+//      getAllVideos {
+//     // try to grab json file
+//     // if have json
+//         // iterate through json file
+//         // add all video names into a list
+//         // return list
+//     // else
+//         // return 404 of no list
+// }
+    getAllVideos(req, res){
+        try {
+            const dataPath = path.join("data", "videos.json");
+              if (!fs.existsSync(dataPath)) {
+                return res.status(404).json({ error: "Video list not found" });
+        }
+         const videos = JSON.parse(fs.readFileSync(dataPath));
+      const names = videos.map(v => v.name);
+      res.status(200).json(names);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+    },
+    
+
+
+//     * getThumbnail
+//     // try to grab video
+//     // if no video
+//     // return 404 of no video
+// send video to jar
+//    // jar tries to return thumbnail
+// if error
+//         // return error status
+//         // else thumbnail
+//         // return status thumbnail
+    getThumbnail(req, res) {
+    const { videoName } = req.params;
+    if (!videoName) return res.status(404).json({ error: "No video name" });
+
+    try {
+      const videoPath = path.join("videos", `${videoName}.mp4`);
+      if (!fs.existsSync(videoPath)) {
+        return res.status(404).json({ error: "Video not found" });
+      }
+    
+      //this line right here is an example jar to get a thumbnail
+       exec(`java -jar videoProcessor.jar thumbnail ${videoPath}`, (err, stdout) => {
+        if (err) return res.status(500).json({ error: "Error generating thumbnail" });
+        res.status(200).json({ thumbnail: stdout });
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+//     * startJob
+//     // try to grab video
+//     // if no video
+//         // return 404 of no video
+//     // else video
+//         // send video to jar
+//         // if error in command
+//             // return bad command status
+//         // jar tries to start process
+//         // if jar starts process
+//             // return jobId status
+//         // return job failed status
+
+//     * getStatus
+//     // if jobId does not exist
+//         // return no jobid status
+//     // if jobId exists
+//         // if progress is error
+//             // return error status
+//         // if progress is processing
+//             // return status of process
+//         // if has csv
+//             // return csv
+    
 }
-
-    * getThumbnail
-    // try to grab video
-    // if no video
-        // return 404 of no video
-    // else video
-        // send video to jar
-        // jar tries to return thumbnail
-        // if error
-            // return error status
-        // else thumbnail
-            // return status thumbnail
-
-    * startJob
-    // try to grab video
-    // if no video
-        // return 404 of no video
-    // else video
-        // send video to jar
-        // if error in command
-            // return bad command status
-        // jar tries to start process
-        // if jar starts process
-            // return jobId status
-        // return job failed status
-
-    * getStatus
-    // if jobId does not exist
-        // return no jobid status
-    // if jobId exists
-        // if progress is error
-            // return error status
-        // if progress is processing
-            // return status of process
-        // if has csv
-            // return csv
-    */
-
-};
+// };
