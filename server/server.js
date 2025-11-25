7651cb429c93269580fc00b431a961bd570b84fc
JesseChum <jessechum@icloud.com>
2025-11-25 14:21:16 -0800

    changed the app.use express to extended

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 import express from "express";
 import { apiRouter } from "./src/routes/api.routes.js";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
 app.set("view engine", "pug");
 app.set("views", "src/views");
-app.use(express.urlencoded({urlencoded: true}));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("./src/public"));
 app.use("/api", apiRouter);
 
+app.use(cors());
 app.listen(PORT, console.log(`http://localhost:${PORT}`));
